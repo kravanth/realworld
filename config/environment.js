7c29ef78c22918aca110ec6dd68ce00bb1bb8c02
kravanth <kranthi.mg2@gmail.com>
2018-03-20 18:03:59 +0530

    hello

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'realworld',
+    modulePrefix: 'super-rental',
     environment,
     rootURL: '/',
     locationType: 'auto',
