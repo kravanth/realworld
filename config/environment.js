e715e0cdd8924401ce42727e25cf33928c022896
kravanth <kranthi.mg2@gmail.com>
2018-03-21 10:46:48 +0530

    configuration mistake bug fixed

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'super-rental',
+    modulePrefix: 'super-rentals',
     environment,
     rootURL: '/',
     locationType: 'auto',
